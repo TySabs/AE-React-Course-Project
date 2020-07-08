2feb712771cd6bc17b900dfae03163c5a9082ac1
Tyler Saballus <tyler.saballus@americaneagle.com>
2020-07-08 12:00:51 -0500

    Lab 4: Your First Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
+import ProjectsPage from "./projects/ProjectsPage";
 import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
-  );
+  return <ProjectsPage />;
 };
 
 export default App;
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProjectsPage() {
+  return <h1>Projects</h1>;
+}
