768aa47f9571c417afdaac7bde5dc18fa19dc17d
Tyler Saballus <tyler.saballus@americaneagle.com>
2020-07-08 23:53:19 -0500

    Lab 8: More Reusable Components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import { Project } from "./Project";
+import ProjectCard from "./ProjectCard";
 
 export default function ProjectList(props: { projects: Project[] }) {
   return (
     <div className="row">
       {props.projects.map((project) => {
-        return (
-          <div className="cols-sm">
-            <div className="card">
-              <img src={project.imageUrl} alt="project name" />
-              <section className="section dark">
-                <h5 className="strong">
-                  <strong>{project.name}</strong>
-                </h5>
-                <p>{project.description}</p>
-                <p>Budget : ${project.budget.toLocaleString()}</p>
-              </section>
-            </div>
-          </div>
-        );
+        return <ProjectCard project={project} />;
       })}
     </div>
   );
