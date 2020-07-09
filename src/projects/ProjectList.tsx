398bd712123d8d6a2db40d77059dc7272cbd9580
Tyler Saballus <tyler.saballus@americaneagle.com>
2020-07-09 11:01:40 -0500

    Lab 11: Communicating from Child to Parent Component

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -4,12 +4,16 @@ import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
 
 export default function ProjectList(props: { projects: Project[] }) {
+  const handleEdit = (project: Project) => {
+    console.log(project);
+  };
+
   return (
     <div className="row">
       {props.projects.map((project, index) => {
         return (
           <div className="cols-sm">
-            <ProjectCard key={index} project={project} />
+            <ProjectCard key={index} project={project} onEdit={handleEdit} />
             <ProjectForm />
           </div>
         );
