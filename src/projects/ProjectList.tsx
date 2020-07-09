266f926e7ea38e2e8c41c8d5718640fdd829b379
Tyler Saballus <tyler.saballus@americaneagle.com>
2020-07-08 23:49:24 -0500

    Lab 7: Displaying List Data

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -3,10 +3,23 @@ import { Project } from "./Project";
 
 export default function ProjectList(props: { projects: Project[] }) {
   return (
-    <>
+    <div className="row">
       {props.projects.map((project) => {
-        return <pre>{JSON.stringify(project, null, " ")}</pre>;
+        return (
+          <div className="cols-sm">
+            <div className="card">
+              <img src={project.imageUrl} alt="project name" />
+              <section className="section dark">
+                <h5 className="strong">
+                  <strong>{project.name}</strong>
+                </h5>
+                <p>{project.description}</p>
+                <p>Budget : ${project.budget.toLocaleString()}</p>
+              </section>
+            </div>
+          </div>
+        );
       })}
-    </>
+    </div>
   );
 }
