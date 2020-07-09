9a4918307b07c257e9ac2525f658debb363b3a1f
Tyler Saballus <tyler.saballus@americaneagle.com>
2020-07-09 10:50:58 -0500

    Lab 10: Creating a Form to Edit Your Data

---
+++ src/projects/ProjectForm.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class ProjectForm extends React.Component {
+  render() {
+    return (
+      <form className="input-group vertical">
+        <label htmlFor="name">Project Name</label>
+        <input type="text" name="name" placeholder="enter name" />
+        <label htmlFor="description">Project Description</label>
+
+        <textarea name="description" placeholder="enter description"></textarea>
+        <label htmlFor="budget">Project Budget</label>
+
+        <input type="number" name="budget" placeholder="enter budget" />
+        <label htmlFor="isActive">Active?</label>
+        <input type="checkbox" name="isActive" />
+
+        <div className="input-group">
+          <button className="primary bordered medium">Save</button>
+          <span></span>
+          <button type="button" className="bordered medium">
+            cancel
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
+import ProjectForm from "./ProjectForm";
 
 export default function ProjectList(props: { projects: Project[] }) {
   return (
     <div className="row">
       {props.projects.map((project, index) => {
-        return <ProjectCard key={index} project={project} />;
+        return (
+          <div className="cols-sm">
+            <ProjectCard key={index} project={project} />
+            <ProjectForm />
+          </div>
+        );
       })}
     </div>
   );
